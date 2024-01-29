dabada2a96e50b481d5d6c2c430dadda453ded12
Maxim_Vod <EMDaGPw+k?3@T9!>
2024-01-29 14:04:13 +0300

    Added a tests for categories

---
+++ test/datebase/categories_test.dart
@@ -0,0 +1,43 @@
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:repit/src/core/components/database/src/app_database.dart';
+import 'package:repit/src/feature/home/data/categories_date_source.dart';
+import 'package:repit/src/feature/home/data/categories_repository.dart';
+import 'package:repit/src/feature/home/model/category_entity.dart';
+
+void main() {
+  late AppDatabase database;
+  late CategoriesDataSource dataSource;
+  late CategoriesRepository repository;
+
+  setUp(() {
+    database = AppDatabase(NativeDatabase.memory());
+    dataSource = CategoriesDao(database);
+    repository = CategoriesRepositoryImpl(dataSource);
+  });
+
+  tearDown(() async {
+    await database.close();
+  });
+
+  group('Test for Categories', () {
+    test('Get all categories test', () async {
+      final list = await repository.getAllCategories();
+      expect(list, isA<List<CategoryEntity>>());
+    });
+
+    test('Create category test', () async {
+      const String firstCategory = "firstCategory";
+      const String secondCategory = "secondCategory";
+      await repository.addCategory(firstCategory);
+      await repository.addCategory(secondCategory);
+      final list = await repository.getAllCategories();
+      final category = list.any((element) => element.name == firstCategory);
+      final firstId = list.first.id;
+      final secondId = list[1].id;
+      expect(category, true);
+      expect(secondId > firstId, true);
+    });
+  });
+}
